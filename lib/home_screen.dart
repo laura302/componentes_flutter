cddc0d1714a4039ea22e31629fe580306be5da73
Laura <lauracontreras19102003@gmail.com>
2024-02-16 10:34:41 -0600

    Adding a simple ListView

---
+++ lib/home_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title:const  Text('Componentes de flutter'),
+      ),
+      body: ListView(
+       children: const [
+        Text('KC Componentes del Super Bowl'),
+        Text('KC Componentes del Super Bowl'),
+        Text('KC Componentes del Super Bowl'),
+        Text('KC Componentes del Super Bowl'),
+        Text('KC Componentes del Super Bowl'),
+        Text('KC Componentes del Super Bowl'),
+        Text('KC Componentes del Super Bowl'),
+        Text('KC Componentes del Super Bowl'),
+        Text('KC Componentes del Super Bowl'),
+        Text('KC Componentes del Super Bowl'),
+        Text('KC Componentes del Super Bowl'),
+        Text('KC Componentes del Super Bowl'),
+        Text('KC Componentes del Super Bowl'),
+        Text('KC Componentes del Super Bowl'),
+        Text('KC Componentes del Super Bowl'),
+        Text('KC Componentes del Super Bowl'),
+        Text('KC Componentes del Super Bowl'),
+        Text('KC Componentes del Super Bowl'),
+       ]
+      ),
+    );
+  }
+}
