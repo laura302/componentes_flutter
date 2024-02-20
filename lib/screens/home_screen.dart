0eb49608875e9fdcc487e35c561f511695c446d9
Laura <lauracontreras19102003@gmail.com>
2024-02-20 12:11:39 -0600

    Adding GoogleFonds and textheme

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:componentes/theme/app_theme.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -7,29 +8,48 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Componentes de flutter'),
+        title: const Text('Componentes de Flutter'),
       ),
       body: ListView(
-        children: const <Widget>[
+        children: <Widget>[
           ListTile(
-            title: Text('Kc campeones del super Bowl 58'),
-            subtitle: Text('Las vegas -11/02/2024'),
-            leading: Icon(Icons.casino),
-            trailing: Icon(Icons.arrow_circle_right_rounded),
+            title: Text(
+              'Entradas',
+              //style:Theme.of(context).textTheme.headlineLarge,
+              style: AppTheme.lightTheme.textTheme.headlineLarge,
+            ),
+            subtitle:Text(
+              'Introducioendo valores',
+              style: AppTheme.lightTheme.textTheme.bodySmall,
+              ),
+            leading: const Icon(Icons.input),
+            trailing: const Icon(Icons.arrow_circle_right_rounded),
           ),
-          Divider(),
+          const Divider(),
           ListTile(
-            title: Text('Kc campeones del super Bowl 57'),
-            subtitle: Text('Arizona -10/02/2023'),
-            leading: Icon(Icons.price_check_sharp),
-            trailing: Icon(Icons.arrow_circle_right_rounded),
+            title: Text(
+              'Listview.builder',
+              style: AppTheme.lightTheme.textTheme.headlineLarge,
+            ),
+            subtitle: Text(
+              'Lista con sroll infinito',
+              style: AppTheme.lightTheme.textTheme.bodySmall
+              ),
+            leading: const Icon(Icons.beach_access),
+            trailing: const Icon(Icons.arrow_circle_right_rounded),
           ),
-          Divider(),
+          const Divider(),
           ListTile(
-            title: Text('Kc campeones del super Bowl 54'),
-            subtitle: Text('Miami -02/02/220'),
-            leading: Icon(Icons.beach_access),
-            trailing: Icon(Icons.arrow_circle_right_rounded),
+            title: Text(
+              'Notificaciones',
+              style: AppTheme.lightTheme.textTheme.headlineLarge,
+              ),
+            subtitle: Text(
+              'Mostar una notificacion ',
+              style: AppTheme.lightTheme.textTheme.bodySmall
+              ),
+            leading: const Icon(Icons.input),
+            trailing: const Icon(Icons.arrow_circle_right_rounded),
           ),
         ],
       ),
--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -1,16 +1,37 @@
-// Definicion de los temas de la App
+//Definicion de los temas de estilo de lapp
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class AppTheme{
-  //Constantes de color primario
-  static const primaryColor = Color.fromARGB(223, 204, 36, 134);
-  // Constante para color de fondo
-  static const backColor = Color.fromARGB(68, 255, 72, 179);
-  //Constante de tema
+class AppTheme {
+  //constante de color primario
+  static const primaryColor = Color.fromARGB(246, 255, 255, 255);
+  // Constante de color secundario
+  static const secondaryColor = Color.fromARGB(245, 0, 0, 0);
+  //constante de color de fondo
+  static const backColor = Color.fromARGB(246, 252, 192, 230);
+  //constante de temas
   static final ThemeData lightTheme = ThemeData.light().copyWith(
     scaffoldBackgroundColor: backColor,
     appBarTheme: const AppBarTheme(
-    color: primaryColor,
-    )
+      color: primaryColor,
+    ),
+    textTheme: TextTheme(
+      //titulos muy grandes
+      headlineLarge: GoogleFonts.abrilFatface(
+        color: primaryColor,
+        fontSize: 26.5,
+        fontWeight: FontWeight.w500,
+        fontStyle: FontStyle.italic,
+        decoration: TextDecoration.underline,
+        decorationColor: primaryColor,
+        decorationStyle: TextDecorationStyle.dotted,
+        decorationThickness: 2.0,
+      ),
+      //Estilo para texto muy pequeño
+      bodySmall: GoogleFonts.alexandria(
+        color: secondaryColor,
+        fontSize: 15.2,
+      ),
+    ),
   );
 }
